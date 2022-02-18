080f762ef106a25b8bef5a0e0678cf948e3db2d3
ThirdAudio <myownkindofmonster@yahoo.com>
2022-02-17 20:54:38 -0600

    Updated button configuration
    Updated buttons with new functionality and added juce_gui_extras to projucer project for JUCE_LIVE_CONSTANT

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -1,10 +1,26 @@
 #include "MainComponent.h"
 
+template<typename ButtonType>
+auto makeButton(ButtonType* buttonToMakeUnique) -> std::unique_ptr<ButtonType>
+{
+    return std::unique_ptr<ButtonType>(buttonToMakeUnique);
+}
+
 //==============================================================================
 MainComponent::MainComponent()
 {
-    addAndMakeVisible(toggleButton);
+    //addAndMakeVisible(toggleButton);
 
+    //ButtonWrapper<juce::TextButton> button(nullptr);
+    /*ButtonWrapper<juce::DrawableButton> button(std::function<void()> onClickHandler,
+                                               const juce::String & name,
+                                               const juce::DrawableButton::ButtonStyle buttonStyle);*/
+    heapButton.reset(new HeapButtonWrapper<juce::TextButton>([](){ DBG("You clicked the heap button"); },
+                                                             new juce::TextButton("HeapButton")
+                                                            )
+                    );
+    addAndMakeVisible(textButton);
+    addAndMakeVisible((*heapButton));
     setSize (600, 400);
 }
 
@@ -28,5 +44,14 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    toggleButton.setBounds(10, 10, 30, 30);
+    //toggleButton.setBounds(10, 10, 30, 30);
+    textButton->setBounds(JUCE_LIVE_CONSTANT(498),
+                          JUCE_LIVE_CONSTANT(0),
+                          100, 
+                          30);
+    //heapButton->operator->()->setBounds(10, 50, 100, 30);
+    (*heapButton)->setBounds(JUCE_LIVE_CONSTANT(498),
+                            JUCE_LIVE_CONSTANT(368),
+                            100,
+                            30);
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,6 +2,78 @@
 
 #include <JuceHeader.h>
 
+template< typename ButtonType>
+struct ButtonWrapper : public juce::Button::Listener
+{
+    template<typename ... Args>
+    ButtonWrapper(std::function<void()> onClickHandler, 
+                  Args&& ... args) : 
+    onClick(std::move(onClickHandler)),
+    button(std::forward<Args>(args) ... )
+    {
+        button.addListener(this);
+    }
+    ~ButtonWrapper() { button.removeListener(this); }
+        
+    void buttonClicked(juce::Button* b) override
+    {
+        if (onClick) 
+            onClick(); 
+    }
+
+    //operator juce::Component*() { return &button; }
+    operator juce::Component&() { return button; }
+    juce::Button* operator->() { return &button; }
+private:
+    std::function<void()> onClick; 
+    ButtonType button;
+};
+
+template< typename ButtonType>
+struct HeapButtonWrapper : public juce::Button::Listener
+{
+    HeapButtonWrapper(std::function<void()> onClickHandler, std::unique_ptr<ButtonType> heapButton) :
+        onClick(onClickHandler),
+        button(std::move(heapButton))
+    {
+        setupButton();
+    }
+
+    HeapButtonWrapper(std::function<void()> onClickHandler, ButtonType* rawButtonPtr) :
+        HeapButtonWrapper(onClickHandler, std::unique_ptr<ButtonType>(rawButtonPtr))
+    {
+        setupButton();
+    }
+    
+    ~HeapButtonWrapper() 
+    { 
+        if (button.get())
+            button->removeListener(this); 
+    }
+
+    void buttonClicked(juce::Button* b) override
+    {
+        if (onClick)
+            onClick();
+    }
+
+    operator juce::Component*() 
+    { 
+        if (button.get() != nullptr )
+            return button.get(); 
+        return nullptr;
+    }
+    juce::Button* operator->() { return button.get(); }
+private:
+    void setupButton() 
+    {
+        jassert(button.get() != nullptr);
+        if (button.get())
+            button->addListener(this);
+    }
+    std::function<void()> onClick;
+    std::unique_ptr<ButtonType> button;
+};
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
@@ -21,7 +93,11 @@ public:
 private:
     //==============================================================================
     // Your private member variables go here...
-    juce::ToggleButton toggleButton;
+    //juce::ToggleButton toggleButton;
+
+    ButtonWrapper<juce::TextButton> textButton{ []() { DBG("You clicked the button!"); }, "textB"};
+    std::unique_ptr<HeapButtonWrapper<juce::TextButton>> heapButton;
+
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
